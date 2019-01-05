a87a9688021810addc77816cea2a96e2a0a36a98
aljohnalbuera <albueraaljohn@gmail.com>
2019-01-05 08:56:42 +0800

    Initial commit

---
+++ Upraxis-Exam/Upraxis-ExamTests/Upraxis_ExamTests.swift
@@ -0,0 +1,34 @@
+//
+//  Upraxis_ExamTests.swift
+//  Upraxis-ExamTests
+//
+//  Created by Al John Albuera on 03/01/2019.
+//  Copyright © 2019 Al John Albuera. All rights reserved.
+//
+
+import XCTest
+@testable import Upraxis_Exam
+
+class Upraxis_ExamTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
