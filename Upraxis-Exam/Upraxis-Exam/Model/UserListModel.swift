26d7fdb8ddc35f758a5680b1556ac31d4b05065f
aljohnalbuera <albueraaljohn@gmail.com>
2019-01-05 22:47:59 +0800

    Added model for the API Data

---
+++ Upraxis-Exam/Upraxis-Exam/Model/UserListModel.swift
@@ -0,0 +1,23 @@
+//
+//  UserListModel.swift
+//  Upraxis-Exam
+//
+//  Created by Al John Albuera on 05/01/2019.
+//  Copyright © 2019 Al John Albuera. All rights reserved.
+//
+
+import UIKit
+
+struct CachedUserInfo {
+    
+    var firstname: String?
+    var lastname: String?
+    var birthday: String?
+    var emailAdd: String?
+    var mobileNumber: Int?
+    var address: String?
+    var contactPerson: String?
+    var contactPersonNumber: Int?
+    
+}
+
